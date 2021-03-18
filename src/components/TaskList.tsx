9863d8a9715fab3ae63f5a8ab3a6da51b4c6a8b7
Rai Medeiros <raismedeiros@gmail.com>
2021-03-17 21:55:44 -0300

    Tasks development for add, remove and complete methods

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,30 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    let id = Math.random()
+    if(!newTaskTitle){
+      console.log("O título deve ser informado")
+      return
+    }
+    const newTask = {
+      id:id,
+      title:newTaskTitle,
+      isComplete:false
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete:!task.isComplete
+    }: task)
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let listTasks = tasks.filter(task=>task.id!==id)
+    setTasks(listTasks)
   }
 
   return (
